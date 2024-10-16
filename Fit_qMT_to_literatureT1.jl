9260300d2ec061c9dc8ed47f7307ee4000c864e7
Jakob Asslaender <jakob.asslaender@nyumc.org>
2024-10-16 19:16:05 -0400

    Added Sled simulation; fixed Julia version to 1.11.1 in github actions

--- Fit_qMT_to_literatureT1.jl
+++ Fit_qMT_to_literatureT1.jl
@@ -12,7 +12,7 @@ p = plot([0, 2], [0, 2], xlabel="simulated T1 (s)", ylabel="literature T1 (s)", 
 marker_list = [(seq_type_i == :IR) ? (:circle) : ((seq_type_i == :LL) ? (:cross) : ((seq_type_i == :vFA) ? (:diamond) : ((seq_type_i == :SR) ? (:dtriangle) : (:x)))) for seq_type_i in seq_type]
 
 fit_name = String[] #src
-fitted_param = NTuple{6, Float64}[] #src
+fitted_param = NTuple{6,Float64}[] #src
 T1_simulated_v = Array{Float64}[] #src
 ΔAIC_v = Float64[] #src
 ΔBIC_v = Float64[] #src
@@ -56,7 +56,7 @@ push!(T1_simulated_v, T1_simulated) #src
 nothing #hide #md
 
 # For this model, the single fitted global of parameter is:
-T1f = 1/fit_mono.param[1] # s
+T1f = 1 / fit_mono.param[1] # s
 # The following plot visualizes the quality of the fit an replicates Fig. 1 in the manuscript:
 scatter!(p, T1_simulated, T1_literature, label="mono-exponential model", markershape=marker_list, hover=seq_name)
 #md Main.HTMLPlot(p) #hide
@@ -120,9 +120,9 @@ nothing #hide #md
 # For this model, the fitted global set of parameters is:
 m0s = fit_Graham.param[1]
 #-
-T1f = 1/fit_Graham.param[2] # s
+T1f = 1 / fit_Graham.param[2] # s
 #-
-T1s = 1/fit_Graham.param[3] # s
+T1s = 1 / fit_Graham.param[3] # s
 # The following plot visualizes the quality of the fit an replicates Fig. 1 in the manuscript:
 scatter!(p, T1_simulated, T1_literature, label="Graham's model (unconstrained R₁ˢ)", markershape=marker_list, hover=seq_name)
 #md Main.HTMLPlot(p) #hide
@@ -136,6 +136,71 @@ n = length(T1_literature)
 k = length(fit_Graham.param)
 #-
 RSS = norm(fit_Graham.resid)^2
+
+# With this information, we can calculate the AIC difference to the mono-exponential model:
+ΔAIC = n * log(RSS / n) + 2k - AIC_mono
+# and similarly for the BIC:
+ΔBIC = n * log(RSS / n) + k * log(n) - BIC_mono
+
+push!(ΔAIC_v, ΔAIC) #src
+push!(ΔBIC_v, ΔBIC) #src
+
+
+#src #########################################################
+# ## Sled's MT model (unconstrained R₁ˢ)
+#src #########################################################
+MT_model = Sled()
+push!(fit_name, "unconstr_Sled") #src
+nothing #hide #md
+
+# The following parameters are hard-coded with the exception of `m0s`, `R1f`, and `R1s`, which serve as an initialization for the global fit.
+m0s = 0.25
+R1f = 1 / 1.84   # 1/s
+R1s = 1 / 0.34   # 1/s
+R2f = 1 / 0.0769 # 1/s
+T2s = 12.5e-6    # s
+Rx = 13.6        # 1/s
+ω0 = 0
+B1 = 1
+nothing #hide #md
+
+# We define a model for the global fit. It takes the global set of parameters `p` as an input and returns a vector of T₁ estimates that correspond to the T₁ mapping methods described by the vector `T1_functions`
+function model(iseq, p)
+    m0s, R1f, R1s = p
+    T1 = similar(iseq, Float64)
+    Threads.@threads for i in eachindex(iseq)
+        T1[i] = T1_functions[iseq[i]](m0s, R1f, R2f, Rx, R1s, T2s)
+    end
+    return T1
+end
+nothing #hide #md
+
+# Perform the fit and calculate the simulated T₁:
+fit_Sled = curve_fit(model, 1:length(T1_literature), T1_literature, [m0s, R1f, R1s], x_tol=1e-3)
+T1_simulated = model(1:length(T1_literature), fit_Sled.param)
+push!(fitted_param, (fit_Sled.param[1], fit_Sled.param[2], R2f, Rx, fit_Sled.param[3], T2s)) #src
+push!(T1_simulated_v, T1_simulated) #src
+nothing #hide #md
+
+# For this model, the fitted global set of parameters is:
+m0s = fit_Sled.param[1]
+#-
+T1f = 1 / fit_Sled.param[2] # s
+#-
+T1s = 1 / fit_Sled.param[3] # s
+# The following plot visualizes the quality of the fit an replicates Fig. 1 in the manuscript:
+scatter!(p, T1_simulated, T1_literature, label="Sled's model (unconstrained R₁ˢ)", markershape=marker_list, hover=seq_name)
+#md Main.HTMLPlot(p) #hide
+
+# Note that clicking on a  legend entry removes the fit from the plot. A double click on an entry selects only this particular fit.
+
+# ### Akaike (AIC) and Bayesian (BIC) information criteria
+# The information criteria depend on the number of measurements `n`, the number of parameters `k`, and the squared sum of the residuals `RSS`:
+n = length(T1_literature)
+#-
+k = length(fit_Sled.param)
+#-
+RSS = norm(fit_Sled.resid)^2
 
 # With this information, we can calculate the AIC difference to the mono-exponential model:
 ΔAIC = n * log(RSS / n) + 2k - AIC_mono
@@ -186,7 +251,7 @@ nothing #hide #md
 # For this model, the fitted global set of parameters is:
 m0s = fit_constr.param[1]
 #-
-T1f = 1/fit_constr.param[2] # s
+T1f = 1 / fit_constr.param[2] # s
 # The following plot visualizes the quality of the fit an replicates Fig. 1 in the manuscript:
 scatter!(p, T1_simulated, T1_literature, label="generalized Bloch model (R₁ˢ = R₁ᶠ constraint)", markershape=marker_list, hover=seq_name)
 #md Main.HTMLPlot(p) #hide
@@ -249,9 +314,9 @@ nothing #hide #md
 # For this model, the fitted global set of parameters is:
 m0s = fit_uncon.param[1]
 #-
-T1f = 1/fit_uncon.param[2] # s
+T1f = 1 / fit_uncon.param[2] # s
 #-
-T1s = 1/fit_uncon.param[3] # s
+T1s = 1 / fit_uncon.param[3] # s
 # The following plot visualizes the quality of the fit an replicates Fig. 1 in the manuscript:
 scatter!(p, T1_simulated, T1_literature, label="generalized Bloch model (unconstrained R₁ˢ)", markershape=marker_list, hover=seq_name)
 #md Main.HTMLPlot(p) #hide
@@ -289,35 +354,41 @@ variation(T1_literature)
 # In the paper, the median absolute deviation from the mean value is used, as it is more robust to outliers compared to the mean absolute deviation or the standard deviation. Note, however, that the median absolute deviation of the mono-exponential fit is dominated by an outlier, artificially inflating the corresponding reduction.
 
 # A mono-exponential model explains the following fraction of the T₁ variability in the literature:
-1 - mad(fit_mono.resid;   center=mean(fit_mono.resid))    / mad(T1_literature; center=mean(T1_literature))
+1 - mad(fit_mono.resid; center=mean(fit_mono.resid)) / mad(T1_literature; center=mean(T1_literature))
 # Graham's spectral model without constraints on `R₁ᶠ` explains the following fraction of the T₁ variability in the literature:
-1 - mad(fit_Graham.resid; center=mean(fit_Graham.resid))  / mad(T1_literature; center=mean(T1_literature))
+1 - mad(fit_Graham.resid; center=mean(fit_Graham.resid)) / mad(T1_literature; center=mean(T1_literature))
+# Sled's model without constraints on `R₁ᶠ` explains the following fraction of the T₁ variability in the literature:
+1 - mad(fit_Sled.resid; center=mean(fit_Sled.resid)) / mad(T1_literature; center=mean(T1_literature))
 # The generalized Bloch model constrained by `R₁ˢ = R₁ᶠ` explains the following fraction of the T₁ variability in the literature:
-1 - mad(fit_constr.resid; center=mean(fit_constr.resid))  / mad(T1_literature; center=mean(T1_literature))
+1 - mad(fit_constr.resid; center=mean(fit_constr.resid)) / mad(T1_literature; center=mean(T1_literature))
 # The generalized Bloch model without constraints on `R₁ᶠ` explains the following fraction of the T₁ variability in the literature:
-1 - mad(fit_uncon.resid;  center=mean(fit_uncon.resid))   / mad(T1_literature; center=mean(T1_literature))
+1 - mad(fit_uncon.resid; center=mean(fit_uncon.resid)) / mad(T1_literature; center=mean(T1_literature))
 
 # ### Mean absolute deviation
 # For comparison, the mean absolute deviation is analyzed:
 # A mono-exponential model explains the following fraction of the T₁ variability in the literature:
-1 - mean(abs.(fit_mono.resid   .- mean(fit_mono.resid)))   / mean(abs.(T1_literature .- mean(T1_literature)))
+1 - mean(abs.(fit_mono.resid .- mean(fit_mono.resid))) / mean(abs.(T1_literature .- mean(T1_literature)))
 # Graham's spectral model without constraints on `R₁ᶠ` explains the following fraction of the T₁ variability in the literature:
 1 - mean(abs.(fit_Graham.resid .- mean(fit_Graham.resid))) / mean(abs.(T1_literature .- mean(T1_literature)))
+# Sled's model without constraints on `R₁ᶠ` explains the following fraction of the T₁ variability in the literature:
+1 - mean(abs.(fit_Sled.resid .- mean(fit_Sled.resid))) / mean(abs.(T1_literature .- mean(T1_literature)))
 # The generalized Bloch model constrained by `R₁ˢ = R₁ᶠ` explains the following fraction of the T₁ variability in the literature:
 1 - mean(abs.(fit_constr.resid .- mean(fit_constr.resid))) / mean(abs.(T1_literature .- mean(T1_literature)))
 # The generalized Bloch model without constraints on `R₁ᶠ` explains the following fraction of the T₁ variability in the literature:
-1 - mean(abs.(fit_uncon.resid  .- mean(fit_uncon.resid)))  / mean(abs.(T1_literature .- mean(T1_literature)))
+1 - mean(abs.(fit_uncon.resid .- mean(fit_uncon.resid))) / mean(abs.(T1_literature .- mean(T1_literature)))
 
 # ### Standard deviation
 # For comparison, the standard deviation is analyzed:
 # A mono-exponential model explains the following fraction of the T₁ variability in the literature:
-1 - std(fit_mono.resid)   / std(T1_literature)
+1 - std(fit_mono.resid) / std(T1_literature)
 # Graham's spectral model without constraints on `R₁ᶠ` explains the following fraction of the T₁ variability in the literature:
 1 - std(fit_Graham.resid) / std(T1_literature)
+# Sled's spectral model without constraints on `R₁ᶠ` explains the following fraction of the T₁ variability in the literature:
+1 - std(fit_Sled.resid) / std(T1_literature)
 # The generalized Bloch model constrained by `R₁ˢ = R₁ᶠ` explains the following fraction of the T₁ variability in the literature:
 1 - std(fit_constr.resid) / std(T1_literature)
 # The generalized Bloch model without constraints on `R₁ᶠ` explains the following fraction of the T₁ variability in the literature:
-1 - std(fit_uncon.resid)  / std(T1_literature)
+1 - std(fit_uncon.resid) / std(T1_literature)
 
 
 #src #########################################################
@@ -374,10 +445,27 @@ println("\$T_1^s\$ constraint     & \\multicolumn{2}{c|}{none}     & \\multicolu
 println("\\midrule") #src
 println("study                   & this                          & \\cite{Gelderen2016,Stanisz.2005}      & this                     & \\cite{Assländer.2024,Stanisz.2005} & this   & \\cite{Assländer.2024oxt} \\\\") #src
 println("\\midrule") #src
-println(@sprintf("\$m_0^s\$ & %1.2f & 0.27 & %1.2f & 0.14 & %1.2f & 0.21 \\\\", fitted_param[2][1], fitted_param[3][1], fitted_param[4][1])) #src
-println(@sprintf("\$T_1^f\$ (s) & %1.2f & 2.44 & %1.2f & 1.52 & %1.2f & 1.84 \\\\", 1/fitted_param[2][2], 1/fitted_param[3][2], 1/fitted_param[4][2])) #src
-println(@sprintf("\$T_1^s\$ (s) & %1.2f & 0.25 & \\g\$T_1^f\$ & & %1.2f & 0.34 \\\\", 1/fitted_param[2][5], 1/fitted_param[4][5])) #src
-println(@sprintf("\$T_2^f\$ (ms) & \\g%1.1f & 69 & \\g%1.1f & 70.1 & \\g%1.1f & 76.9 \\\\", 1e3/fitted_param[2][3], 1e3/fitted_param[3][3], 1e3/fitted_param[4][3])) #src
-println(@sprintf("\$T_2^s\$ (\$\\upmu\$s) & \\g%1.1f & 10.0 & \\g%1.1f & & \\g%1.1f & 12.5 \\\\", 1e6*fitted_param[2][6], 1e6*fitted_param[3][6], 1e6*fitted_param[4][6])) #src
-println(@sprintf("\$R_\\text{x}\$ (s\$^{-1}\$) & \\g%1.1f & 9.0 & \\g%1.1f & 23.0 & \\g%1.1f & 13.6 \\\\", fitted_param[2][4], fitted_param[3][4], fitted_param[4][4])) #src
+println(@sprintf("\$m_0^s\$ & %1.2f & 0.27 & %1.2f & 0.14 & %1.2f & 0.21 \\\\", #src
+    fitted_param[findfirst(fit_name .== "unconstr_Graham")][1], #src
+    fitted_param[findfirst(fit_name .== "constr_gBloch")][1], #src
+    fitted_param[findfirst(fit_name .== "unconstr_gBloch")][1])) #src
+println(@sprintf("\$T_1^f\$ (s) & %1.2f & 2.44 & %1.2f & 1.52 & %1.2f & 1.84 \\\\", #src
+    1 / fitted_param[findfirst(fit_name .== "unconstr_Graham")][2], #src
+    1 / fitted_param[findfirst(fit_name .== "constr_gBloch")][2], #src
+    1 / fitted_param[findfirst(fit_name .== "unconstr_gBloch")][2])) #src
+println(@sprintf("\$T_1^s\$ (s) & %1.2f & 0.25 & \\g\$T_1^f\$ & & %1.2f & 0.34 \\\\", #src
+    1 / fitted_param[findfirst(fit_name .== "unconstr_Graham")][5], #src
+    1 / fitted_param[findfirst(fit_name .== "unconstr_gBloch")][5])) #src
+println(@sprintf("\$T_2^f\$ (ms) & \\g%1.1f & 69 & \\g%1.1f & 70.1 & \\g%1.1f & 76.9 \\\\", #src
+    1e3 / fitted_param[findfirst(fit_name .== "unconstr_Graham")][3], #src
+    1e3 / fitted_param[findfirst(fit_name .== "constr_gBloch")][3], #src
+    1e3 / fitted_param[findfirst(fit_name .== "unconstr_gBloch")][3])) #src
+println(@sprintf("\$T_2^s\$ (\$\\upmu\$s) & \\g%1.1f & 10.0 & \\g%1.1f & & \\g%1.1f & 12.5 \\\\", #src
+    1e6 * fitted_param[findfirst(fit_name .== "unconstr_Graham")][6], #src
+    1e6 * fitted_param[findfirst(fit_name .== "constr_gBloch")][6], #src
+    1e6 * fitted_param[findfirst(fit_name .== "unconstr_gBloch")][6])) #src
+println(@sprintf("\$R_\\text{x}\$ (s\$^{-1}\$) & \\g%1.1f & 9.0 & \\g%1.1f & 23.0 & \\g%1.1f & 13.6 \\\\", #src
+    fitted_param[findfirst(fit_name .== "unconstr_Graham")][4], #src
+    fitted_param[findfirst(fit_name .== "constr_gBloch")][4], #src
+    fitted_param[findfirst(fit_name .== "unconstr_gBloch")][4])) #src
 println("\\bottomrule") #src

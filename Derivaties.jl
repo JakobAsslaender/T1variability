e293b8fdd9a424c1dfcac6a6b7d77d545c9d98e3
Jakob Asslaender <jakob.asslaender@nyumc.org>
2025-08-01 17:05:36 -0400

    Analyze fixed models with Shapley

--- Derivaties.jl
+++ Derivaties.jl
@@ -6,6 +6,7 @@ using CSV
 using MixedModels
 using StatsPlots
 using Printf
+using Combinatorics
 plotlyjs(bg=RGBA(31 / 255, 36 / 255, 36 / 255, 1.0), ticks=:native, size=(800, 1000))
 MT_model = gBloch()
 calculate_jacobian = false
@@ -111,11 +112,14 @@ scatter(df[!, q], df[!, d], group=df.seq_type, hover=df.seq_name, xlabel=string(
 
 
 ## Linear Mixed model
-str_contributions = ""
-str_contributions_tex = "\$\\partial T_1/\\partial p_i^\\text{MT}\$ & \$\\mu(\\frac{\\partial T_1}{\\partial p_i^\\text{MT}}) \\cdot \\mu(p_i^\\text{MT})\$ & \$\\sigma(\\frac{\\partial T_1}{\\partial p_i^\\text{MT}}) / \\mu(\\frac{\\partial T_1}{\\partial p_i^\\text{MT}})\$ & \$R^2_\\text{fixed}\$ & \$R^2_\\text{ROI}\$ & \$R^2_\\text{seq. type}\$ & \$R^2_\\text{seq. name}\$ & \$R^2_\\text{full}\$ & \$m_0^s = 0\$ \\\\"
+include("Derivatives_HelperFunctions.jl")
 
-str_fixed = ""
-str_fixed_tex = ""
+str_r2 = ""
+str_r2_tex = ""
+str_fixed_r2 = ""
+str_fixed_r2_tex = ""
+str_fixed_model = ""
+str_fixed_model_tex = ""
 j_tex = ["\$\\partial T_1 / \\partial m_0^s\$", "\$\\partial T_1 / \\partial T_1^f\$", "\$\\partial T_1 / \\partial T_2^f\$", "\$\\partial T_1 / \\partial T_\\text{x}\$", "\$\\partial T_1 / \\partial T_1^s\$", "\$\\partial T_1 / \\partial T_2^s\$"]
 
 
@@ -147,24 +151,20 @@ for id ∈ eachindex(j)
     fe = fixef(model)
     m0s_intercept = fe[1] + mean(df.T1f) * fe[3] + mean(df.T2f) * fe[4] + mean(df.Tex) * fe[5] + mean(df.T1s) * fe[6] + mean(df.T2s) * fe[7]
 
-    str_contributions *= @sprintf("%s: mean_j = %.3f; CoV = %.3f; r²_fixed = %.3f; r²_ROI = %.3f; r²_seqtype = %.3f; r²_seqname = %.3f; r²_full = %.3f; \n", j[id], mj, cv, r²_fixed, r²_ROI, r²_seqtype, r²_seqname, r²_full)
-    str_contributions_tex *= @sprintf("%s & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f \\\\ \n", j_tex[id], mj, cv, r²_fixed, r²_ROI, r²_seqtype, r²_seqname, r²_full)
+    str_r2 *= @sprintf("%s: mean_j = %.3f; CoV = %.3f; r²_fixed = %.3f; r²_ROI = %.3f; r²_seqtype = %.3f; r²_seqname = %.3f; r²_full = %.3f; \n", j[id], mj, cv, r²_fixed, r²_ROI, r²_seqtype, r²_seqname, r²_full)
+    str_r2_tex *= @sprintf("%s & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f \\\\ \n", j_tex[id], mj, cv, r²_fixed, r²_ROI, r²_seqtype, r²_seqname, r²_full)
 
+    Δr² = shapley_regression(df, j[id], p)
+    str_fixed_r2 *= @sprintf("%s: %.3f; %.3f; %.3f; %.3f; %.3f; %.3f; %.3f \\\\ \n", j[id], Δr²..., sum(Δr²))
+    str_fixed_r2_tex *= @sprintf("%s & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f \\\\ \n", j_tex[id], Δr²...)
 
-    # r²_m0s = var(model.βs[:m0s] .* m0s, corrected=false) / σ²_all
-    # r²_T1f = var(model.βs[:T1f] .* T1f, corrected=false) / σ²_all
-    # r²_T2f = var(model.βs[:T2f] .* T2f, corrected=false) / σ²_all
-    # r²_Tex = var(model.βs[:Tex] .* Tex, corrected=false) / σ²_all
-    # r²_T1s = var(model.βs[:T1s] .* T1s, corrected=false) / σ²_all
-    # r²_T2s = var(model.βs[:T2s] .* T2s, corrected=false) / σ²_all
-
-    # str_fixed *= @sprintf("%s; %.3f; %.3f; %.3f; %.3f; %.3f; %.3f; %.3f \\\\ \n", j[id], r²_m0s, r²_T1f, r²_T2f, r²_Tex, r²_T1s, r²_T2s, m0s_intercept)
-    # str_fixed_tex *= @sprintf("%s & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f \\\\ \n", j_tex[id], r²_m0s, r²_T1f, r²_T2f, r²_Tex, r²_T1s, r²_T2s, m0s_intercept)
+    str_fixed_model *= @sprintf("%s: %.3f; %.3f; %.3f; %.3f; %.3f; %.3f; %.3f; %.3f \\\\ \n", j[id], fe..., m0s_intercept)
+    str_fixed_model_tex *= @sprintf("%s & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f \\\\ \n", j_tex[id], fe..., m0s_intercept)
 
 
     # Plot: observed vs full-model prediction
-    dlim = maximum(df[!, :d]) - minimum(df[!, :d])
-    xlim = (minimum(df[!, :d]) - 0.15dlim, maximum(df[!, :d]) + 0.15dlim)
+    dlim = maximum(df[!, j[id]]) - minimum(df[!, j[id]])
+    xlim = (minimum(df[!, j[id]]) - 0.15dlim, maximum(df[!, j[id]]) + 0.15dlim)
     ylim = xlim
 
     scatter(df[!, j[id]], pred, group=df.seq_type, hover=df.seq_name,
@@ -176,11 +176,14 @@ for id ∈ eachindex(j)
     display(current())
 end
 
+println(str_r2)
+println(str_fixed_r2)
+println(str_fixed_model)
 
-println(str_contributions)
-println(str_fixed)
-# println(str_contributions_tex)
-# println(str_fixed_tex)
+##
+println(str_r2_tex)
+println(str_fixed_r2_tex)
+println(str_fixed_model_tex)
 
 ## Export CSV for plotting in latex
 _df = deepcopy(df)
@@ -192,11 +195,3 @@ for id ∈ eachindex(j)
 end
 
 CSV.write(expanduser("~/Documents/Paper/2025_T1sensitivity/Figures/derivatives/jacobian.csv"), _df)
-
-
-
-
-##
-# using MixedModelsExtras
-# r2(model; conditional=false)
-# r2(model; conditional=true)
---
+++ Derivatives_HelperFunctions.jl
@@ -0,0 +1,39 @@
+# Analyze fixed models with Shapley
+function shapley_regression(df, lhs, p)
+    Δr² = similar(p, Float64)
+    Δr² .= 0
+
+    for ipick ∈ eachindex(p)
+        p_pick = p[ipick]
+        p_rest = filter(x -> x != p_pick, p)
+        r_terms = term(1) + (term(1) | term(:seq_type)) + (term(1) | term(:seq_name)) + (term(1) | term(:ROI))
+
+        for nterms = 0:length(p_rest)
+            weight = 1 / length(combinations(p_rest, nterms)) / (length(p_rest) + 1)
+            for combination ∈ combinations(p_rest, nterms)
+                rhs = combination == Symbol[] ? r_terms : sum(t -> term(t), combination) + r_terms
+                r²_rest = _r2(df, term(lhs), rhs)
+
+                rhs += term(p_pick)
+                r²_pick = _r2(df, term(lhs), rhs)
+
+                Δr²[ipick] += weight * (r²_pick - r²_rest)
+            end
+        end
+    end
+    return Δr²
+end
+
+function _r2(df, lhs, rhs)
+    model = fit(MixedModel, lhs ~ rhs, df)
+    fixedeffects = Symbol.(rhs[collect(typeof.(rhs) .== Term)])
+
+    σ²_fixed = isempty(fixedeffects) ? 0 : var(sum(param -> model.βs[param] .* df[!, param], fixedeffects); corrected=false)
+    σ²_ROI = model.σs[:ROI][1]^2
+    σ²_seqname = model.σs[:seq_name][1]^2
+    σ²_seqtype = model.σs[:seq_type][1]^2
+    σ²_ϵ = model.σ^2
+    σ²_all = σ²_fixed + σ²_ROI + σ²_seqtype + σ²_seqname + σ²_ϵ
+    r² = σ²_fixed / σ²_all
+    return r²
+end
